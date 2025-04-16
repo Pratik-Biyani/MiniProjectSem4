86c7ff9fb06162f72e33cbf333c0bb0464041157
Hardik-M-Agarwal <hardikag04@gmail.com>
2025-04-17 02:02:54 +0530

    reset votes fix

--- reset_votes.py
+++ reset_votes.py
@@ -6,7 +6,7 @@ def reset_votes():
 
     cursor.execute("UPDATE users SET voted = 0") 
     cursor.execute("UPDATE voters SET has_voted = 'No'")  
-    cursor.execute("UPDATE votes1 SET vote_count = 0")  
+    cursor.execute("UPDATE votes2 SET vote_count = 0")  
     conn.commit()
     conn.close()
 
